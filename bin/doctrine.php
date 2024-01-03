b762c1eaaf46a8dd23cae470ac6d3c3cdfd873e7
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2024-01-03 17:29:59 +0100

    chore: use a separate container file

--- bin/doctrine.php
+++ bin/doctrine.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-require_once __DIR__.'/../vendor/autoload.php';
-
-defined('DM_ROOT') or exit('Configuration is not loaded.');
-
 use Distantmagic\Resonance\DoctrineConsoleRunner;
 
-DoctrineConsoleRunner::run();
+$container = require_once __DIR__.'/../container.php';
+
+exit(DoctrineConsoleRunner::run($container));
--- bin/resonance.php
+++ bin/resonance.php
@@ -2,19 +2,8 @@
 
 declare(strict_types=1);
 
-require_once __DIR__.'/../vendor/autoload.php';
-
-defined('DM_ROOT') or exit('Configuration is not loaded.');
-
 use Distantmagic\Resonance\ConsoleApplication;
-use Distantmagic\Resonance\DependencyInjectionContainer;
-use Swoole\Runtime;
 
-Runtime::enableCoroutine(SWOOLE_HOOK_ALL);
-
-$container = new DependencyInjectionContainer();
-$container->phpProjectFiles->indexDirectory(DM_RESONANCE_ROOT);
-$container->phpProjectFiles->indexDirectory(DM_APP_ROOT);
-$container->registerSingletons();
+$container = require_once __DIR__.'/../container.php';
 
 exit($container->make(ConsoleApplication::class)->run());
